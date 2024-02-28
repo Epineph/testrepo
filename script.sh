8d75b09104bce01e843ff6accf655b1bbe257eaa
Epineph <44166929+Epineph@users.noreply.github.com>
2024-02-28 04:21:19 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,54 +1,72 @@
 #!/bin/bash
 
+#!/bin/bash
 
 
-echo "ipadurin koyrir command 'apk' (stendur fyri alpine linux package) commands :"
-sleep 2
+# Ensure the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
-echo "koyrir nu comands 'apk update og apk upgrade'"
-sleep 2
+# Ask for the username
+read -p "Enter the username you wish to add: " USERNAME
 
-apk update
+# Add the new user (you might want to add additional options as needed)
+adduser $USERNAME
 
-apk upgrade
+# Add user to the wheel group for sudo privileges
+addgroup $USERNAME wheel
 
-"echo installerar bash um tu ikki longu hevur"
-sleep 2
+# Update apk and upgrade existing packages
+apk update && apk upgrade
 
-apk add bash
+# Install core utilities and sudo
+
+apk add bash coreutils sudo shadow vim curl wget git openssh openssh-askpass openssh-client-common aconf-mod-openssh acf-openssh openssh-client-default openssh-dbg
+apk add openssh-doc openssh-keygen openssh-keysign openssh-server openssh-server-commmon openssh-server-common-openrc openssh-server-krb5 openssh-server-pam
+apk add openssh-sftp-server openssh-sk-helper perl-net-openssh perl-net-openssh-doc lxqt-openssh-askpass lxqt-openssh-askpass-doc lxqt-openssh-askpass-lang
 
 
-echo "fyri at kanna hvorjar shells tu hevur, kanst tu koyra command: 'cat /etc/shells'"
 
-sleep 2
+# Setup a non-root user (replace 'myuser' with your desired username)
 
-echo "hesar shells hevur tu nu:"
-sleep 1
+echo '$USERNAME ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/$USERNAME
+chmod 0440 /etc/sudoers.d/$USERNAME
 
-cat /etc/shells
 
-sleep 2
+# Setup locale (Alpine uses musl-locales)
+apk add musl-locales musl-locales-lang
+echo 'export LANG=en_DK.UTF-8' >> /etc/profile
+echo 'export LANGUAGE=en_DK.UTF-8' >> /etc/profile
+echo 'export LC_ALL=C.UTF-8' >> /etc/profile
+source /etc/profile
 
-echo "downloadar bash completion"
+# Optional: Install additional packages as needed
+# apk add <package_name>
+
+apk add linux-headers make perl-utils perl-error sudo
+
+# Install sudo and setup sudoers
+echo "Setting up sudo..."
+apk add sudo
+echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/wheel
+chmod 0440 /etc/sudoers.d/wheel
+
+
 
 apk add bash-completion
+
+apk add zsh-completion
 
 
 cat /etc/bash/bash_completion.sh
 
-echo "geri brukari philip"
-echo "um tad ikki riggar, so stendur ein alternativur mati i scriptinum"
-
-
 apk add sudo
-NEWUSER='philip'
+NEWUSER='hwj'
 adduser -g "${NEWUSER}" $NEWUSER
 echo "$NEWUSER ALL=(ALL) ALL" > /etc/sudoers.d/$NEWUSER && chmod 0440 /etc/sudoers.d/$NEWUSER
 
-
-
-echo "downloadar zsh"
-sleep 2
 
 
 apk add zsh
@@ -75,6 +93,12 @@ ln -s /usr/share/zsh/plugins/powerlevel10k ~/.local/share/zsh/plugins/
 
 apk add shadow git
 
-touch /home/philip/.bashrc
+touch $USERNAME/.bashrc
 
-echo "alias update='apk update && apk upgrade'" | sudo tee -a /home/philip/.bashrc
+echo "alias update='apk update && apk upgrade'" | sudo tee -a $USERNAME/.bashrc
+echo "ipadurin koyrir command 'apk' (stendur fyri alpine linux package) commands :"
+sleep 2
+
+echo "koyrir nu comands 'apk update og apk upgrade'"
+sleep 2
+
